33cd5aa964f03212725c4a79871c1f952dfc0bea
holyjohn00 <zgadiane@gmail.com>
2017-04-14 18:48:10 +0800

    Added Dao

--- LivingWater/src/main/java/com/livingwater/dao/BatchBottlesDao.java
+++ LivingWater/src/main/java/com/livingwater/dao/BatchBottlesDao.java
@@ -5,7 +5,7 @@ import com.livingwater.entities.User;
 import org.springframework.stereotype.Repository;
 import org.springframework.transaction.annotation.Transactional;
 
-@Repository("batchBottles")
+@Repository("batchBottlesDao")
 @Transactional
 public interface BatchBottlesDao extends GenericDao<BatchBottles>{
 
